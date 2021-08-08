17ca33ebca5774f1c004c4ca7d335264c0bb3479
Alex Mills <alex@acm.io>
2021-08-08 13:11:28 +0800

    Hardcode Macos window to 9inch display

--- plano/macos/Runner/MainFlutterWindow.swift
+++ plano/macos/Runner/MainFlutterWindow.swift
@@ -4,8 +4,11 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
+    var windowFrame = self.frame
     self.contentViewController = flutterViewController
+
+    // Includes 30px for Macos Title Bar
+    windowFrame.size = CGSize.init(width: 1280, height: 830)
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
