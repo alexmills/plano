435279153b13367102867cc1c6fab73264f9699b
Alex Mills <alex@acm.io>
2021-09-02 19:06:17 +0800

    Added Settings State Store

--- plano/lib/main.dart
+++ plano/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plano/app.dart';
 import 'package:plano/stores/director.dart';
 import 'package:plano/stores/scene.dart';
+import 'package:plano/stores/settings.dart';
 import 'package:plano/stores/theme.dart';
 import 'package:provider/provider.dart';
 
@@ -11,6 +12,7 @@ void main() {
       ChangeNotifierProvider(create: (context) => DirectorStore()),
       ChangeNotifierProvider(create: (context) => SceneStore()),
       ChangeNotifierProvider(create: (context) => ThemeStore()),
+      ChangeNotifierProvider(create: (context) => SettingsStore()),
     ],
     child: PlanoApp(),
   ));
